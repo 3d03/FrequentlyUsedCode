c554b67a07cdf28375827ee021177b98fe0d2572
alexey ayusheev <ayusheev@lb24.ru>
2019-03-29 07:10:40 +0800

    escListener and ToggleDependableHider

---
+++ FrequentlyUsedCode/Assets/Scripts/UI/EditorUI/ToggleDependableHider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ToggleDependableHider: MonoBehaviour
+{
+    [SerializeField]
+    public bool flag;
+    [SerializeField]
+    public int i = 1;
+
+}
+
+
+[CustomEditor(typeof(ToggleDependableHider))]
+public class MyScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var myScript = target as ToggleDependableHider;
+
+        myScript.flag = GUILayout.Toggle(myScript.flag, "Flag");
+
+        if (myScript.flag)
+            myScript.i = EditorGUILayout.IntSlider("I field:", myScript.i, 1, 100);
+
+    }
+}
---
+++ FrequentlyUsedCode/Assets/Scripts/UI/EscListener.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button ))]
+public class EscListener : MonoBehaviour {
+
+    public bool isAppCloser;
+    public void CloseApp()
+    {
+        if (isAppCloser)
+        {
+            Application.Quit();
+        }
+
+    }
+	void Update () {
+	
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isAppCloser)
+            {
+                Application.Quit();
+            }
+            else
+            { 
+                gameObject.GetComponent<Button>().onClick.Invoke();
+            }
+        }
+	}
+}
