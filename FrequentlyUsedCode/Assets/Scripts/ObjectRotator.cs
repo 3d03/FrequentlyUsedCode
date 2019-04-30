7c351a35bf764854fa33186a5824c1c330d3a4c7
Aleksei Aiusheev a.k.a. Alexey Ayusheev <3d03@mail.ru>
2019-05-01 02:22:51 +0800

    ObjectRotator.cs    some old script

---
+++ FrequentlyUsedCode/Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class ObjectRotator : MonoBehaviour 
+{
+ 
+    private float _sensitivity;
+    public Vector3 _mouseReference;
+    public Vector3 _mouseOffset;
+    public Vector3 _rotation;
+    public bool _isRotating;
+ 
+    void Start ()
+    {
+       _sensitivity = 0.4f;
+       _rotation = Vector3.zero;
+    }
+ 
+    void Update()
+    {
+       if(_isRotating)
+       {
+         // offset
+         _mouseOffset = (Input.mousePosition - _mouseReference);
+ 
+         // apply rotation
+         //_rotation.x = (_mouseOffset.y) * _sensitivity;
+		 _rotation.y = -(_mouseOffset.x) * _sensitivity;
+			
+ 
+         // rotate
+         transform.Rotate(_rotation);
+ 
+         // store mouse
+         _mouseReference = Input.mousePosition;
+       }
+    }
+ 
+    void OnMouseDown()
+    {
+       // rotating flag
+       _isRotating = true;
+ 
+       // store mouse
+       _mouseReference = Input.mousePosition;
+    }
+ 
+    void OnMouseUp()
+    {
+       // rotating flag
+       _isRotating = false;
+    }
+ 
+}
