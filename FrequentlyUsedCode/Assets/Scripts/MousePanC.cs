3a46fc465120c5638e02de42e7dc922c9e8cd986
Aleksei Aiusheev a.k.a. Alexey Ayusheev <3d03@mail.ru>
2019-05-01 02:18:08 +0800

    pan zoom scripts

---
+++ FrequentlyUsedCode/Assets/Scripts/MousePanC.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePanC : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float mouseSensitivity=1.0f;
+	private Vector3 lastPosition;
+	private Vector3 delta;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ((Input.GetMouseButtonDown(0))||(Input.GetMouseButtonDown(1))||(Input.GetMouseButtonDown(2)))
+		{
+			lastPosition = Input.mousePosition;
+		}
+		
+		if ((Input.GetMouseButton(0))||(Input.GetMouseButton(1))||(Input.GetMouseButton(2)))
+		{
+			delta = Input.mousePosition - lastPosition;
+			transform.Translate(delta.x * mouseSensitivity, delta.y * mouseSensitivity, 0);
+			lastPosition = Input.mousePosition;
+		}
+	}
+}
+
+
+
---
+++ FrequentlyUsedCode/Assets/Scripts/MouseZoom.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseZoom : MonoBehaviour {
+	public Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetAxis("Mouse ScrollWheel")<0)
+		{
+
+			if (mainCamera.fieldOfView<=133)
+			    {
+				mainCamera.fieldOfView+=2;
+				}
+
+
+
+		}
+
+
+		if (Input.GetAxis("Mouse ScrollWheel")>0)
+		{
+			
+			if (mainCamera.fieldOfView>=20)
+			{
+				mainCamera.fieldOfView-=2;
+			}
+			
+			
+			
+		}
+
+
+	}
+}
