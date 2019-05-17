ab548028429c43929a1f1ea73b1ec15259650452
alexey ayusheev <ayusheev@lb24.ru>
2019-05-18 03:47:22 +0800

    upd

---
+++ FrequentlyUsedCode/Assets/Scripts/Transformation/RotateTowards.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour
+{
+    private Transform thisTransform=null;
+    public float RotSpeed = 90f;
+    public Transform Target = null;
+    void Start()
+    {
+        thisTransform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion DestRot = Quaternion.LookRotation(Target.position - thisTransform.position, Vector3.up);
+        thisTransform.rotation = Quaternion.RotateTowards(thisTransform.rotation, DestRot, RotSpeed * Time.deltaTime);
+    }
+}
