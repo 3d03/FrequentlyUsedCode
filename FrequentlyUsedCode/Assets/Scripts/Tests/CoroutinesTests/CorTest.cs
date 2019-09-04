aa202dbde184d848e0fe7d2d79816b0deb7cf59d
alexey ayusheev <3d03@mail.ru>
2019-09-04 23:08:53 +0800

    1

---
+++ FrequentlyUsedCode/Assets/Scripts/Tests/CoroutinesTests/CorTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorTest : MonoBehaviour
+{
+
+    public Vector3 dir;
+    public GameObject mover;
+    float speed = 2f;
+
+
+    void Start()
+    {
+        StartCoroutine(test());
+    }
+    IEnumerator test( )
+    {
+        float movedDist = 0;
+        while (movedDist<15f)
+        {
+            //movedDist += speed * Time.deltaTime;
+            //mover.transform.Translate(Vector3.up * speed * Time.deltaTime);
+            movedDist += speed * 0.4f;
+            mover.transform.Translate(dir * speed * 0.4f);
+           // yield return new WaitForEndOfFrame();
+            yield return new WaitForSeconds(0.4f);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
